06477cd3ee2a207005b5b7990b2bb598dbb91c5e
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-04-12 16:34:56 +0200

    milestones fatte

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 /* main */
 let container = document.getElementById("container");
+let postDiv = document.createElement("div");
+let likedPost = [];
 generaPost();
+/* creazione evento mi piace */
+let like = document.querySelectorAll(".like-button");
+for(let i = 0; i < like.length; i++){
+    like[i].addEventListener("click", function(){
+        this.classList.add("like-button--liked");
+        posts[i].likes++;
+        let counter = posts[i].likes;
+        console.log(counter)
+        let counterLike = document.querySelectorAll(".js-likes-counter");
+        counterLike[i].innerHTML = counter;
+        if(!likedPost.includes(posts[i].id)){
+            likedPost.push(posts[i].id)
+        }
+        console.log(likedPost)
+    });
+    
+}
 
-/* genera post */
+
+
+/* funzione mi piace */
+/* function getLikes(i){
+    this.classList.add("like-button--liked");
+    console.log(this)
+} */
+
+/*funzione genera post */
 function generaPost(){
+
     posts.forEach((post)=>{
         container.innerHTML += `
         
@@ -26,7 +54,7 @@ function generaPost(){
                 <div class="post__footer">
                     <div class="likes js-likes">
                         <div class="likes__cta">
-                            <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                            <a class="like-button  js-like-button" data-postid="${post.id}">
                                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                 <span class="like-button__label">Mi Piace</span>
                             </a>
@@ -39,6 +67,6 @@ function generaPost(){
             </div>
 
         `;
-
     })
+
 }
